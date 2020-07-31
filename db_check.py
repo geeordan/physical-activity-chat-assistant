70f1519cd37a30a1591f64dc7109eecea86a8212
geeordan <58528692+geeordan@users.noreply.github.com>
2020-07-30 18:12:42 -0700

    Delete db_check.py

--- db_check.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cur = conn.cursor()
-cur.execute("SELECT * FROM users")
-print(cur.fetchall())
-cur.close()
-conn.close()
